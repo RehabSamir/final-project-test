ba8c6c0fe198d62d6bc2dee5d71622e9000a0b12
abdElrahman <abdelrahmanelnobby@gmail.com>
2024-08-14 15:21:03 +0300

    abdo cafee

---
+++ src/app/services/service5.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Service5Service {
+
+  constructor() { }
+
+  abdo(){
+    console.log(" let's make a very good cup of tea")
+  }
+
+  abdo2(){
+    console.log(" let's make a very good cup of cafee")
+
+  }
+}
